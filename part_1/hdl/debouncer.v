4f79f8a8753c43f277c9fd4c23b4ed5343143a94
Cilantrop <114765990+Cilantrop@users.noreply.github.com>
2022-11-18 22:01:23 -0500

    debouncer

--- part_1/hdl/debouncer.v
+++ part_1/hdl/debouncer.v
@@ -20,8 +20,64 @@ module debouncer (
   output wire                   led7
 
 );
+endmodule
 
-// Your lab2 part-1 code comes here
-
+module top_module(
+    input button_in,
+    input clk_in,
+    input resetn_in,
+    output reg[7:0] count
+    );
+    wire w1;
+    
+        debouncer d1(w1, clk_in, button_in, resetn_in);
+        counter c1(count, clk_in, w1, resetn_in);
 
 endmodule
+
+
+module counter(
+    input clk_in,
+    input button_in,
+    input resetn_in,
+    output reg[7:0] count = 1'b00000000
+    );
+    always @ (posedge button_in or negedge resetn_in) begin
+        if (resetn_in == 1'b1) begin
+            count <= 0;
+        end else
+            count <= count + 1'b00000001;
+    end//always block  
+endmodule
+
+
+module debouncer(
+
+    input clk_in,
+    input button_in,
+    input resetn_in,
+    output button_out
+    );
+    parameter MAX = 10; //10ms at 25Mhz
+    reg button_on = 1'b0;
+    reg [17:0] counter = 0;
+        always @ (posedge clk_in or negedge resetn_in) begin
+            if (resetn_in == 1'b1) begin
+                button_on <= 1'b0;
+                counter <= 0;
+            end else if ( button_in !== button_on && counter < MAX) begin
+                counter <= counter + 1;
+            end else if (counter == MAX) begin
+                counter <= 0;
+                button_on <= button_in;
+            end else
+                counter <= 0;
+        end//always block
+    assign button_out = button_on;
+endmodule
+
+
+
+
+
+
